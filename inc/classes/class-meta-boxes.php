f924548f3938acb23d2e4666e90e953bbfb80af8
“Osaro <saro.iyoha@gmail.com>
2025-10-06 21:17:11 +0100

    Add nonce_field to meta-box before saving to db

--- inc/classes/class-meta-boxes.php
+++ inc/classes/class-meta-boxes.php
@@ -41,6 +41,12 @@ class Meta_Boxes {
 	}}
 	public function custom_meta_box_html($post){
 	$value = get_post_meta( $post->ID, '_hide_page_title', true );
+
+	/**
+	 * Create & Use nonce for verification 
+	 */
+
+	wp_nonce_field(plugin_basename(__FILE__), 'hide_title_meta_box_nonce_name')
 		?>
 	<label for="aquila-field"><?php esc_html_e('Hide the page title', 'aquila'); ?></label>
 	<select name="aquila_hide_title_field" id="aquila-field" class="postbox">
@@ -52,6 +58,23 @@ class Meta_Boxes {
 	}
 
 	function save_post_meta_data( $post_id ) {
+		/**
+		 * When the post is saved or updated we get $_POST available
+		 * Check if current user is authorized
+		 */
+
+		if(!current_user_can('edit_post', $post_id)){
+			return;
+		}
+
+		/**
+		 * Check if nonce value we received is same as the one created
+		 */
+
+		if(!isset($_POST['hide_title_meta_box_nonce_name']) || ! wp_verify_nonce($_POST['hide_title_meta_box_nonce_name'], plugin_basename(__FILE__))){
+			return ;
+		}
+
 	if ( array_key_exists( 'aquila_hide_title_field', $_POST ) ) {
 		update_post_meta(
 			$post_id,

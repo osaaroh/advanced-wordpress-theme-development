eb21ceb73e6fd4060955cd4653b20e779e5a30e8
“Osaro <saro.iyoha@gmail.com>
2025-10-07 21:41:31 +0100

    Add posted date

--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -36,3 +36,29 @@ return $custom_thumbnail;
 function the_post_custom_thumbnail($post_id, $size='featured-thumbnail', $additional_attributes = []){
     echo get_the_post_custom_thumbnail($post_id, $size, $additional_attributes );
 }
+
+function aquila_posted_on() {
+    //$time_string = '<time class = "entry-date published updated" datetime="%1$s">%2$s</time>';
+
+    //get_the_time - gets the time current post was written
+    //get_the_modified_time - gets the time current post was modified
+
+    //Post is modified
+    if(get_the_time('U') !== get_the_modified_time('U')){
+        $time_string = '<time class = "entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+    }
+
+    $time_string = sprintf($time_string,
+        esc_attr(get_the_date(DATE_W3C)),
+        esc_attr(get_the_date()),
+        esc_attr(get_the_modified_date(DATE_W3C)),
+        esc_attr(get_the_modified_date())
+    );
+
+    $posted_on = sprintf(
+        esc_html_x('Posted on %s', 'post date', 'aquila'),
+        '<a href="'. esc_url(get_permalink()). '" rel="bookmark">'. $time_string . '</a>'
+    );
+
+    echo '<span class="posted-one text-secondary">'. $posted_on.'</span>';
+}

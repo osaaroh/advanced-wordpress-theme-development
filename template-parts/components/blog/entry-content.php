7ed7f799e2a7a9ea56a00982fd1bf8288692e4d8
“Osaro <saro.iyoha@gmail.com>
2025-10-08 20:39:34 +0100

    Add excerpt to blog list views

--- template-parts/components/blog/entry-content.php
+++ template-parts/components/blog/entry-content.php
@@ -1,11 +1,35 @@
 <?php
 /**
- * Content Template
+ * Template for entry content
+ * 
+ * 
+ * To be used inside wordpress The Loop.
  * 
  * @package Aquila
- * 
  */
 ?>
 
 <h3><?php //the_title(); ?></h3>
-<div><?php the_excerpt(); ?></div>
+<div><?php // the_excerpt(); ?></div>
+
+<div class="entry-content">
+    <?php 
+        if (is_single()) {
+            the_content(
+                sprintf(
+                    wp_kses(
+                        __('Continue reading %s <span class="meta-nav">&rarr;</span>', 'aquila'),
+                        [
+                            'span' => [
+                                'class' => []
+                            ]
+                        ]
+                            ),
+                            the_title('<span class="screen-reader-text">"', '"</span>', false)
+                )
+            );
+        } else {
+            aquila_the_excerpt(150);
+        }
+    ?>
+</div>

db5b60308615a914106420d71f1e4a0302405cae
“Osaro <saro.iyoha@gmail.com>
2025-11-10 15:55:38 +0100

    WP: Add Cusrom  WP block pattern categories

--- inc/classes/class-block-patterns.php
+++ inc/classes/class-block-patterns.php
@@ -21,6 +21,7 @@ class Block_Patterns {
 	protected function setup_hooks(){
 		//actions and filters
         add_action('init', [$this, 'register_block_patterns']);
+        add_action('init', [$this, 'register_block_pattern_categories']);
 	}
 
     public function register_block_patterns(){
@@ -30,6 +31,7 @@ class Block_Patterns {
                 [
                     'title'=>__('Aquila Cover', 'aquila'),
                     'description'=>__('Aquila Cover Block with image and text', 'aquila'),
+                    'categories' => ['cover'],
                     'content' => '<!-- wp:cover {"url":"https://my-first-theme.local/wp-content/uploads/2025/11/wallpaperflare.com_wallpaper-2-1024x576.jpg","id":114,"dimRatio":50,"customOverlayColor":"#706f6f","isUserOverlayColor":false,"sizeSlug":"large","layout":{"type":"constrained"}} -->
 <div class="wp-block-cover"><img class="wp-block-cover__image-background wp-image-114 size-large" alt="" src="https://my-first-theme.local/wp-content/uploads/2025/11/wallpaperflare.com_wallpaper-2-1024x576.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim" style="background-color:#706f6f"></span><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1,"fontSize":"large"} -->
 <h1 class="wp-block-heading has-text-align-center has-large-font-size">Never let your  memories be greater than your dreams</h1>
@@ -49,4 +51,21 @@ class Block_Patterns {
             );
         }
     }
+
+    public function register_block_pattern_categories(){
+        $pattern_categories = [
+            'cover' => __('Cover', 'aquila'),
+            'carousel' => __('Carousel', 'aquila'),
+        ];
+
+        if (!empty($pattern_categories) && is_array($pattern_categories)) {
+            foreach ($pattern_categories as $pattern_catergory => $pattern_category_label){
+                register_block_pattern_category(
+                    $pattern_catergory,
+                    array('label'=> $pattern_category_label)
+                );    
+            } 
+        }
+    }
 }
+
